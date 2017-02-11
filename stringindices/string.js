9a26c8a57f1bc7e69384bcba9c6f95cb5447fff7
lisabronwyn <e.bronwyn@gmail.com>
2017-02-10 17:50:40 -0800

    finished jsonobject and worked on stringindices

---
+++ stringindices/string.js
@@ -0,0 +1,12 @@
+function stringedIndexes(string, index) {
+    let newIndex = string.split('') {
+        if (index > newIndex.length) {
+            return " "
+        } else {
+            return newIndex
+        }
+        if (index < 0) {
+            return " "
+        }
+    }
+  }
